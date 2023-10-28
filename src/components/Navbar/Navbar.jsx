14de4ae6349a2a831bffd34392619e13c74fef6a
Samson Agbo <agbosamsonite@gmail.com>
2023-10-28 16:48:39 +0100

    fixed issues on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-
 import { images } from '../../constants';
 import './Navbar.scss';
-
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+
+  const handleOnClick = (e) => {
+    e.stopPropagation();
+    setToggle((prevState) => !prevState);
+  };
 
   return (
     <nav className="app__navbar">
@@ -16,33 +19,41 @@ const Navbar = () => {
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div/>
+            <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
 
       <div className="app__navbar-menu">
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+        <HiMenuAlt4 onClick={(e) => handleOnClick(e)} />
 
-          {toggle && (
-            <motion.div
-              whileInView={{ x: [300,0] }}
-              transition={{ duration: 0.85, ease: 'easeOut'}}
+        {toggle && (
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: 300 }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <motion.span
+              initial={{ width: 0 }}
+              animate={{ width: 70 }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
             >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                  <li key={{item}}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
+              <HiX onClick={(e) => handleOnClick(e)} />
+            </motion.span>
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
       </div>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;
